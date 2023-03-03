fb76f3335a620f3eba57a8be9fa844f873041964
Mesele <getmesy@gmail.com>
2023-03-03 13:40:57 +0000

    push advanced tasks

---
+++ 123-avl_remove.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * avl_remove - removes a node from an AVL tree
+ * @root: a pointer to the root node of the tree for removing a node
+ * @value: the value to remove in the tree
+ *
+ * Return: a pointer to the new root node of the tree after removal or
+ * NULL on failure
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+
+}
